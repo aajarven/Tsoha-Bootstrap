345e0c5d680d2ba73fdd664e4083d9595ffa0553
aajarven <anni.jarvenpaa@gmail.com>
2016-11-12 00:13:21 +0200

    suunnitelmille pohjat

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,50 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  //View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        //View::make('home.html');
         echo 'Etusivu =3';
     }
 
-    public static function sandbox(){
+    public static function sandbox() {
         View::make('helloworld.html');
     }
-  }
+
+    public static function etusivu() {
+        View::make('suunnitelmat/etusivu.html');
+    }
+
+    public static function opettajanakyma() {
+        View::make('suunnitelmat/opettajanakyma.html');
+    }
+
+    public static function opiskelijanakyma() {
+        View::make('suunnitelmat/opiskelijanakyma.html');
+    }
+
+    public static function laitosnakyma() {
+        View::make('suunnitelmat/laitosnakyma.html');
+    }
+    
+    public static function tiedekuntanakyma() {
+        View::make('suunnitelmat/tiedekuntanakyma.html');
+    }
+    
+    public static function tulokset_opettaja() {
+        View::make('suunnitelmat/tulokset-opettaja.html');
+    }
+    
+    public static function tulokset_hallinto() {
+        View::make('suunnitelmat/tulokset-hallinto.html');
+    }
+    
+    public static function muokkaus_kysely() {
+        View::make('suunnitelmat/muokkaus-kysely.html');
+    }
+    
+    public static function muokkaus_kysymys() {
+        View::make('suunnitelmat/muokkaus-kysymys.html');
+    }
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,45 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/etusivu', function() {
+    HelloWorldController::etusivu();
+});
+
+$routes->get('/opettajanakyma', function() {
+    HelloWorldController::opettajanakyma();
+});
+
+$routes->get('/opiskelijanakyma', function() {
+    HelloWorldController::opiskelijanakyma();
+});
+
+$routes->get('/laitosnakyma', function() {
+    HelloWorldController::laitosnakyma();
+});
+
+$routes->get('/tiedekuntanakyma', function() {
+    HelloWorldController::tiedekuntanakyma();
+});
+
+$routes->get('/tulokset-opettaja', function() {
+    HelloWorldController::tulokset_opettaja();
+});
+
+$routes->get('/tulokset-hallinto', function() {
+    HelloWorldController::tulokset_hallinto();
+});
+
+$routes->get('/muokkaus-kysely', function() {
+    HelloWorldController::muokkaus_kysely();
+});
+
+$routes->get('/muokkaus-kysymys', function() {
+    HelloWorldController::muokkaus_kysymys();
+});
