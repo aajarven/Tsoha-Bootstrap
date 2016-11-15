9894608c552565fc293afebee45c23d39d3c7989
aajarven <anni.jarvenpaa@gmail.com>
2016-11-15 05:37:51 +0200

    joillekin tauluille testidataa. Sellaisille en osannut lisätä, joiden pitäisi viitata johonkin autoinkrementoituun

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,10 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (sahkoposti, salasanaHash, suola) VALUES ('proffa.fiksunen@helsinki.fi', 'asdfgasdfsadfasdf', 'suolaa');
+INSERT INTO Kayttaja (sahkoposti, salasanaHash, suola) VALUES ('oppilas.hononen@helsinki.fi', 'asdfgasdfsadfasdsdfaf', 'suolia');
+INSERT INTO Kayttaja (sahkoposti, salasanaHash, suola) VALUES ('hallintokaytava@helsinki.fi', 'afdssdfgasdfsadfasdf', 'suoleja');
+
+INSERT INTO KayttajaRyhma (nimi) VALUES ('opiskelija');
+INSERT INTO KayttajaRyhma (nimi) VALUES ('opettaja');
+
+INSERT INTO Organisaatio (nimi) VALUES ('tietojenkäsittelytieteen laitos');
+INSERT INTO Organisaatio (nimi) VALUES ('fysiikan laitos');
+INSERT INTO Organisaatio (nimi) VALUES ('matemaattis-luonnontieteellinen tiedekunta');
